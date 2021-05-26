f8ed11160ed8d7f3c2d5741ca09782c30f1f0eb2
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-05-26 10:28:24 +0300

    Add project files.

---
+++ Stack Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(input);
+
+            while (true)
+            {
+                string command = Console.ReadLine().ToLower();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                string[] commandArguments = command.Split();
+
+                string currentCommand = commandArguments[0];
+
+
+                if (currentCommand == "add")
+                {
+                    stack.Push(int.Parse(commandArguments[1]));
+                    stack.Push(int.Parse(commandArguments[2]));
+                }
+                else
+                {
+                    if (stack.Count < int.Parse(commandArguments[1]))
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < int.Parse(commandArguments[1]); i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine("\r\n" + "The sum is: " + stack.Sum());
+        }
+    }
+}
